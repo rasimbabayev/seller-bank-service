c43c4233d90500099f55bec12c2e5b3b05411876
Rasim Babayev <rasim10babayev@gmail.com>
2023-10-20 11:37:55 +0200

    init

---
+++ src/test/java/com/reausnta/selllerbank/SelllerBankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reausnta.selllerbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelllerBankServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
